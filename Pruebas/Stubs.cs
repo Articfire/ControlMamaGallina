5857eec8721598c02f76360b881a052defd7d6d5
Martin Hurtado <martindrumer21@gmail.com>
2022-06-11 15:05:30 -0700

    Merge Pog

---
+++ Pruebas/Stubs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Clase_DPS1
+{
+    public class LoginStubs
+    {
+        public object[] Seleccionar(string query)
+        {
+            return new object[3] {"adminm", "1410", "Administrador" };
+        }
+
+        public bool isUserLoginValid(object[] resultados, string user_field, string password_field)
+        {
+            return true;
+        }
+    }
+}
